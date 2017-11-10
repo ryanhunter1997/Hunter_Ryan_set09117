e4f55b69cfdc688a1cbc2b3c3b59d58356004210
ryanhunter1997 <40206280@live.napier.ac.uk>
2017-11-10 17:03:45 +0000

    board now prints, jump function started

--- coursework.py
+++ coursework.py
@@ -1,62 +1,79 @@
-#!/user/bin/python3
-from enum import Enum                                                                                                                                            
+#!/usr/bin/python3
+from enum import Enum
+
 
 CLEAR = lambda: os.system('cls' if os.name == 'nt' else 'clear')
 GRID_HEIGHT = 8
-GRID_WIDTH  = 8
-#white pawn
-WP = "w"
-#white queen
-WQ = "W"
-#black pawn
-BP = "b"
-#black queen
-BQ = "B"
-#empty cell
-EC = " "
+GRID_WIDTH = 8
 
-PALYERS = Enum("Players", "White  Black")
-#End of constants
+WP = 'w'
+WQ = 'W'
+EC = '_'
+BP = 'b'
+BQ = 'B'
+
+PLAYERS = Enum("Players", "White Black")
 
 def main():
-    CLEAR()
-    print("PY-CHECKERS")
-    value_package = dict([("board", init_grid()), ("turn_count", 1), ("cur_turn", PLAYERS.white)])
+    print("Python Checkers")
+    value_package = dict([("board", init_board()), ("turn_count", 1), ("cur_turn", PLAYERS.White)])
     while True:
-        move(value_package)
+        print_board(value_package["board"])
+        jump(value_package, ["board"])
 
-def init_grid():
-    grid =[[EC, BP, EC, BP, EC, BP, EC, BP],
-           [BP, EC, BP, EC, BP, EC, BP, EC],
-           [EC, BP. EC, BP, EC, BP, EC, BP],
-           [EC, EC, EC, EC, EC, EC, EC, EC],
-           [EC, EC, EC, EC, EC, EC, EC, EC],
-           [WP, EC, WP, EC, WP, EC, WP, EC],
-           [EC, WP, EC, WP, EC, WP, EC, WP],
-           [WP, EC, WP, EC, WP, EC, WP ,EC]]
-    return grid
+def init_board():
+    board=[
+            ['w', '_', 'w', '_', 'w', '_', 'w', '_'],
+            ['_', 'w', '_', 'w', '_', 'w', '_', 'w'],
+            ['w', '_', 'w', '_', 'w', '_', 'w', '_'],
+            ['_', '_', '_', '_', '_', '_', '_', '_'],
+            ['_', '_', '_', '_', '_', '_', '_', '_'],
+            ['_', 'b', '_', 'b', '_', 'b', '_', 'b'],
+            ['b', '_', 'b', '_', 'b', '_', 'b', '_'],
+            ['_', 'b', '_', 'b', '_', 'b', '_', 'b']]
+    return board
 
 def print_board(board):
-    print("      A B C D E F G H I J\n")
-    for i in range(GRID_HEIGHT):
-        print(i, " |", end=="")
+    print("     1 2 3 4 5 6 7 8 \n")
+    for i in range (GRID_HEIGHT):
+        print(i,  "  |", end="")
         for j in range(GRID_WIDTH):
-            current_cell = board[i][j]
-            print(current_cell + "|", end=="")
-        print ("")
+            print(board[i][j]  + "|", end="")
+        print("")
     print("")
 
-def move(value_package):
-    print("Turn : ", value_package["turn_count"])
-    if value_package["cur_turn"] == PLAYERS.White:
-        print("White's turn :\n")
-        print_board(value_package["board"])
-        while True:
-            print("Enter movement :", ends=="")
-            if interpret_response(value_package["board2"], input()) == True:
-                value_package["cur_turn"] = PLAYERS.Black
-                value_package["turn_count"] +=1
-                break
-    else:
-        print("Black's turn :\n")
-        value_package["cur_turn"] = PLAYERS.White
+
+def jump(value_package, board):
+    src_x = int(input("Choose a x coord to move from "))
+    src_y = int(input("Choose a y coord to move from "))
+    dst_x = int(input("Choose a y coord to move to "))
+    dst_y = int(input("Choose a y coord to move to "))
+
+
+    x_diff = abs(src_x - dst_X)
+    y_diff = abs(src_x - dst_y)
+
+    if sorted([x_diff, y_diff]) != [0, 2]:
+        print('invalid coordinates, try again.')
+        return
+
+    mid_x = (src_x + dst_x) // 2
+    mid_y = (src_y + dst_y) // 2
+
+    if board[src_y][src_x] == '_':
+        print('Source cell is empty, try again.')
+
+    if board[dst_y][dst_x] != '_':
+        print('Target cell is taken, try again.')
+
+    if board[mid_y][mid_x] == '_':
+        print('No piece to jump over')
+
+    if board[src_y][src_x] == board[mid_y][mid_x]:
+        print('Cannot take your own piece')
+
+    board[dst_y][dst_x] = board[src_y][src_x]
+    board[mid_y][mid_x] = '_'
+    board[src_y][src_x] = '_'
+
+main()
